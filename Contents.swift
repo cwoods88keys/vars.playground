d7ad297833cae6ccdc95ba8f3ab3e52e2d09cd3f
cwoods88keys <prc-lh138-14@PRC-LH138-14s-iMac.local>
2016-07-11 14:11:09 -0500

    varsConstants

---
+++ Contents.swift
@@ -0,0 +1,30 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var aVariable: Int = 32
+var bVariable = 32
+var cVariable: Float = 32
+
+let aConstant: Double = 32.134
+
+print(aVariable)
+print(cVariable)
+
+print ("a: \(aVariable)  b:\(bVariable)")
+
+print (aVariable, terminator: "") //removes \n
+
+//let bConstant = (Double)bVariable / 10.0 //doesn't work
+
+let pi = 3.14
+
+var radius = 3.0
+print("area: ", pi * radius * radius)
+
+
+
+
+
